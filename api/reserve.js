02ecbd19e9112c21548b77143cf92ef84c2db760
Ayan Arsalan <160471222+cheesesocrates@users.noreply.github.com>
2025-08-13 00:48:41 +0500

    Update reserve.js

--- api/reserve.js
+++ api/reserve.js
@@ -1,5 +1,3 @@
-reserve.js
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ success: false, message: 'Method Not Allowed' });
@@ -22,3 +20,4 @@ export default async function handler(req, res) {
     res.status(500).json({ success: false, message: err.message });
   }
 }
+
